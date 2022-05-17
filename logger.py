c9ef549ba7033af464b8c1706287a6df6a122aa6
EmTeedee <git.emteedee@das-komitee.de>
2022-05-17 11:07:49 +0200

    configurable logging

--- logger.py
+++ logger.py
@@ -1,14 +1,25 @@
 import logging
+import yaml
+
+with open("config.yml", "r") as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+
+# get log level from config
+log_level = getattr(logging, cfg["log"]["level"].upper(), None)
+if not isinstance(log_level, int):
+    raise ValueError('Invalid log level: %s' % cfg["log"]["level"])
 
 logger = logging.getLogger('kodiplex')
-level = logging.INFO
-logger.setLevel(level)
-# create file handler which logs even debug messages
+logger.setLevel(log_level)
+
+# create file handler which logs up to debug messages
 fh = logging.FileHandler('kodiplex.log')
-fh.setLevel(level)
-# create console handler with a higher log level
+fh.setLevel(logging.DEBUG)
+
+# create console handler which logs up to info
 ch = logging.StreamHandler()
-ch.setLevel(level)
+ch.setLevel(logging.INFO)
+
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
